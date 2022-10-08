07d7ed40cd419a28f6c1c9ee6be3d47e1aecfac8
ewerton <ewerton.lima@sptech.school>
2022-10-08 19:52:56 -0300

    atualizado porem com bug de css nao pula para pagina com dashboard funcionado

---
+++ Public/Front_End && Back_End/Front_End && Back_End/JavaScript/Script.js
@@ -0,0 +1,12 @@
+
+const apiKey = "213718a018ff31e1c645b901213ec212"
+const apiCoutryURL = "https://countryflagsapi.com/png/"
+
+const cityInput = document.querySelector("#city")
+const searchBtn = document.querySelector("#search")
+
+
+searchBtn.addEventListener("click" , (e) =>{
+    e.preventDefault();
+    console.log("ola")
+})
